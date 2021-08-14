53c849c6ba582d5ce0060a320a9cd2418239f8c0
Rabius Sunny <rabiussunnyc3x@gmail.com>
2021-08-14 22:34:31 +0600

    navbar and footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,9 +1,13 @@
+import { Toolbar } from "@material-ui/core"
+
 const Footer = () => {
-    return (
-        <div>
-            footer
-        </div>
-    )
+    return <div style={{ position: 'sticky' }}>
+        <Toolbar style={{ justifyContent: 'center', background: '#dc004e', color: 'whitesmoke' }}>
+            {/* eslint-disable-next-line */}
+            <p style={{ fontFamily: 'sans-serif' }}>Copyright opened | Share articles with mentioning sources and without any corruption <a style={{ border: '1px solid white', padding: '5px 10px', borderRadius: '15px', margin: '0 10px' }} href="#">^</a></p>
+        </Toolbar>
+    </div>
+
 }
 
 export default Footer
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,9 +1,27 @@
+import Link from 'next/link'
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import HomeRoundedIcon from '@material-ui/icons/HomeRounded';
+
 const Navbar = () => {
+
     return (
-        <div>
-            nav
+        <div style={{ flexGrow: 1 }}>
+            <AppBar position="static">
+                <Toolbar>
+                    <Link href="/" passHref>
+                        <IconButton edge="start" color="inherit" aria-label="menu">
+                            <HomeRoundedIcon />
+                        </IconButton>
+                    </Link>
+                    <div style={{ flexGrow: 1 }}></div>
+                    <Link href="/tags" passHref><Button color="inherit">Tags</Button></Link>
+                </Toolbar>
+            </AppBar>
         </div>
-    )
+    );
 }
 
 export default Navbar
