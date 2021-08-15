ccffa30952a75d931469edaae9bb6ad336371e50
Rabius Sunny <rabiussunnyc3x@gmail.com>
2021-08-15 16:35:10 +0600

    SSR post details

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,11 @@ import * as React from 'react';
 import PostPagination from '../src/pagination';
 import { RootContainer } from '../styles/customs/wrappers';
 
-export default function Index({ posts }: any) {
+export default function Index({ data }: any) {
+
+  let posts: any[] = []
+  data.map((post: {}) => posts.unshift(post))
+
   return (
     <RootContainer>
       <PostPagination
@@ -15,13 +19,11 @@ export default function Index({ posts }: any) {
 }
 
 export async function getServerSideProps() {
-  
+
   const res = await fetch('https://mbook-backend.herokuapp.com/sharedposts')
   const data = await res.json()
-  let posts: any[] = []
-  data.map((post: {}) => posts.unshift(post))
 
   return {
-    props: { posts }
+    props: { data }
   }
 }
--- pages/post/[postId].tsx
+++ pages/post/[postId].tsx
@@ -1,37 +1,44 @@
 import { Container } from "@material-ui/core"
+import { useRouter } from "next/router"
 
 const DetailPost = ({ posts }: any) => {
 
-    const { title, post } = posts[0]
-    return (
-        <Container>
-            <h1>{title}</h1>
-            <p>{post}</p>
-        </Container>
-    )
+    const router = useRouter()
+
+    if (router.isFallback) {
+        return <h1>Loading</h1>
+    } else {
+        const { title, post } = posts[0]
+        return (
+            <Container>
+                <h1>{title}</h1>
+                <p>{post}</p>
+            </Container>
+        )
+    }
 }
 
 export default DetailPost
 
-// export async function getStaticPaths() {
-//     const res = await fetch('https://mbook-backend.herokuapp.com/sharedposts')
-//     const data = await res.json()
+export async function getStaticPaths() {
+    const res = await fetch('https://mbook-backend.herokuapp.com/sharedposts')
+    const data = await res.json()
 
-//     const paths = data.map((post: any) => {
-//         return {
-//             params: {
-//                 postId: `${post._id}`
-//             }
-//         }
-//     })
+    const paths = data.map((post: any) => {
+        return {
+            params: {
+                postId: `${post._id}`
+            }
+        }
+    })
 
-//     return {
-//         paths,
-//         fallback: true
-//     }
-// }
+    return {
+        paths,
+        fallback: true
+    }
+}
 
-export async function getServerSideProps(context: any) {
+export async function getStaticProps(context: any) {
     let { params } = context
 
     const res = await fetch(`https://mbook-backend.herokuapp.com/sharedpost/${params.postId}`)
